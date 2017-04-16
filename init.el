560e5e4e5d4349e1d9f9cb2a75ccca6888ffb7d6
hatsusato <knightspm5@gmail.com>
2017-04-17 02:58:21 +0900

    Manage packages for each version

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
+;; パッケージをバージョンごとに管理する。
+(let ((versioned-dir (locate-user-emacs-file emacs-version)))
+  (setq el-get-dir (expand-file-name "el-get" versioned-dir)
+        package-user-dir (expand-file-name "elpa" versioned-dir)))
+
 ;; el-getをインストールする。
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
