b8ace531e29384ffe31fba9360a0dc8625a54b95
hatsusato <knightspm5@gmail.com>
2017-05-10 18:28:01 +0900

    Add default-theme.el

--- custom.el
+++ custom.el
@@ -27,6 +27,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4feaff1b8ff9c5fd8aa54114f3ad5893f1e84ab4dbfea48911653b4f2f3e50fa" default)))
  '(package-selected-packages
    (quote
     (zenburn-theme volatile-highlights undohist undo-tree smooth-scroll smartparens rainbow-delimiters multiple-cursors markdown-mode magit hlinum helm gitignore-mode gitconfig-mode flycheck bind-key auto-complete ace-jump-mode))))
---
+++ default-theme.el
@@ -0,0 +1,2 @@
+(deftheme default "The default color theme")
+(provide-theme 'default)
