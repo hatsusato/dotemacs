9ecda953d6385371c5a9441f406291057482d1f9
hatsusato <knightspm5@gmail.com>
2017-07-03 11:08:10 +0900

    Fix ace-jump-mode-move-keys

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
 (use-package ace-jump-mode
   :config
   (setq ace-jump-mode-move-keys
-        (append "asdfghjkl;qwertyuiop" nil))
+        (append "asdfghjkl;qwertyuiopzxcvbnm,./" nil))
   (setq ace-jump-word-mode-use-query-char nil)
   :bind
   ("M-:" . ace-jump-char-mode)
