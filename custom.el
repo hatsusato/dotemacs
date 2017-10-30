791fcfc2cd8165fdd39e1e341b5271a87bce7b63
hatsusato <knightspm5@gmail.com>
2017-10-30 23:42:55 +0900

    Fix custom-safe-themes

--- custom.el
+++ custom.el
@@ -34,7 +34,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4528fb576178303ee89888e8126449341d463001cb38abe0015541eb798d8a23" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default))))
+    ("a58cc789698ca7c1059749f08baccd51702f53a7bf757509c3a1153769f0f60d" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
